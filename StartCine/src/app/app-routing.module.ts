99fabbdf227b36cc4c308341de138160579eeaae
Meowbutu <mewmewdevart@gmail.com>
2023-08-29 21:32:14 -0300

    fix + refactor : bootstrap workando e slider-home sobrevivendo na base da gambiarra

--- StartCine/src/app/app-routing.module.ts
+++ StartCine/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { HomeComponent } from './home/home.component';
 import { RodapeDadosComponent } from './rodape-dados/rodape-dados.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'rodape-dados', component: RodapeDadosComponent}
+  { path: '', component: HomeComponent },
+  { path: 'rodape-dados', component: RodapeDadosComponent }
 ];
 
 @NgModule({
